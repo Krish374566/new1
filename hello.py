4ad99d4d118e076d32fa16f20738be42ea98f43d
Krish374566 <sm374566@gmail.com>
2026-04-18 15:25:01 +0530

    rename Dockerfile again again

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(_name_)
 
 @app.route("/")
 def hello():
-    return "Hello from krishna"
+    return "Hello from krishna Maurya"
 
 if _name_ == "_main_":
     app.run(host="0.0.0.0", port=8000)
